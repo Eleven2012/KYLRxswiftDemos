8400fa9200df8c5060a8e665139ba29d3b03f974
yulu.kong <yulu.kong@ubtrobot.com>
2019-08-08 15:12:08 +0800

    过滤条件操作符

--- 002-RxSwift核心销毁者/002-RxSwift核心销毁者/ViewController.swift
+++ 002-RxSwift核心销毁者/002-RxSwift核心销毁者/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 import RxSwift
 import RxCocoa
 
+struct LGPlayer {
+    init(score: Int) {
+        self.score = BehaviorSubject(value: score)
+    }
+    let score: BehaviorSubject<Int>
+}
+
+extension Date {
+    static var time: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH-mm-ss"
+        return formatter.string(from: Date())
+    }
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var startAction: UIButton!
@@ -24,74 +39,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
  //        limitObservable()
 //          intervalObservableTest()
-        test_switchLatest()
+        test_skipUntil()
     }
     
     
     deinit {
         print("我走了")
     }
-}
-
-
-// MARK: - Rx高阶函数： 映射操作符
-extension ViewController {
-    func test_map() {
-        
-    }
-    
-    func test_flatmap() {
-        
-    }
-    
-    func test_scan() {
-        
-    }
-}
-
-
-// MARK: - Rx高阶函数： 过滤条件操作符
-extension ViewController {
-    func test_fliter() {
-        
-    }
-    
-    func test_distinctUntilChanged() {
-        
-    }
-    
-    func test_elementAt() {
-        
-    }
-    
-    func test_single() {
-        
-    }
-    
-    func test_take() {
-        
-    }
-    
-    func test_takeLast() {
-        
-    }
-    
-    func test_takeUntil() {
-        
-    }
-    
-    func test_skip() {
-        
-    }
-    
-    func test_skipWhile() {
-        
-    }
-    
-    func test_skipUntil() {
-        
-    }
-    
 }
 
 
@@ -146,6 +100,263 @@ extension ViewController {
     }
     
 }
+
+
+
+// MARK: - Rx高阶函数： 过滤条件操作符
+extension ViewController {
+    ///仅从满足指定条件的可观察序列中发出那些元素
+    func test_fliter() {
+        // **** filter : 仅从满足指定条件的可观察序列中发出那些元素
+        print("*****filter*****")
+        Observable.of(1,2,3,4,5,6,7,8,9,0)
+            .filter{$0 % 2 == 0}
+            .subscribe(onNext: {print($0)})
+            .disposed(by: disposeBag)
+    }
+    
+    ///抑制可观察序列发出的顺序重复元素
+    func test_distinctUntilChanged() {
+        // ***** distinctUntilChanged: 抑制可观察序列发出的顺序重复元素
+        print("*****distinctUntilChanged*****")
+        Observable.of("1", "2", "2", "2", "3", "3", "4")
+            .distinctUntilChanged()
+            .subscribe(onNext: {print($0)})
+            .disposed(by: disposeBag)
+    }
+    
+    ///仅在可观察序列发出的所有元素的指定索引处发出元素
+    func test_elementAt() {
+        // **** elementAt: 仅在可观察序列发出的所有元素的指定索引处发出元素
+        print("*****elementAt*****")
+        Observable.of("C", "o", "o", "c", "i")
+            .elementAt(3)
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposeBag)
+    }
+    
+    ///只发出可观察序列发出的第一个元素(或满足条件的第一个元素)。如果可观察序列发出多个元素，将抛出一个错误。
+    func test_single() {
+        // *** single: 只发出可观察序列发出的第一个元素(或满足条件的第一个元素)。如果可观察序列发出多个元素，将抛出一个错误。
+        print("*****single*****")
+        Observable.of("kongyulu", "yuhairong")
+            .single()
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposeBag)
+    }
+    
+    func test_single2() {
+        // *** single: 只发出可观察序列发出的第一个元素(或满足条件的第一个元素)。如果可观察序列发出多个元素，将抛出一个错误。
+        print("*****single*****")
+        Observable.of("kongyulu", "yuhairong")
+            .single{ $0 == "kongyulu"}
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposeBag)
+    }
+    ///只从一个可观察序列的开始发出指定数量的元素。 上面signal只有一个序列 在实际开发会受到局限 这里引出 take 想几个就几个
+    func test_take() {
+        // **** take: 只从一个可观察序列的开始发出指定数量的元素。 上面signal只有一个序列 在实际开发会受到局限 这里引出 take 想几个就几个
+        print("*****take*****")
+        Observable.of("kongyulu", "yuhairong","yifeng", "yisheng")
+            .take(2)//这里取前面两个
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposeBag)
+    }
+    
+    func test_take2() {
+        // **** take: 只从一个可观察序列的开始发出指定数量的元素。 上面signal只有一个序列 在实际开发会受到局限 这里引出 take 想几个就几个
+        print("*****take*****")
+        Observable.of("kongyulu", "yuhairong","yifeng", "yisheng")
+            .take(3)//这里取前面三个
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposeBag)
+    }
+    
+    //仅从可观察序列的末尾发出指定数量的元素
+    func test_takeLast() {
+        // *** takeLast: 仅从可观察序列的末尾发出指定数量的元素
+        print("*****takeLast*****")
+        Observable.of("kongyulu", "yuhairong","yifeng", "yisheng")
+            .takeLast(3)//取从末尾开始算起的3个元素
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposeBag)
+    }
+    
+    ///只要指定条件的值为true，就从可观察序列的开始发出元素
+    func test_takeWhile() {
+        // **** takeWhile: 只要指定条件的值为true，就从可观察序列的开始发出元素
+        print("*****takeWhile*****")
+        Observable.of(1, 2, 3, 4, 5, 6)
+            .takeWhile { $0 < 3 } //取出满足条件的元素 (1,2)
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposeBag)
+    }
+    
+    /// 从源可观察序列发出元素，直到参考可观察序列发出元素
+    /// 这个要重点,应用非常频繁 比如我页面销毁了,就不能获取值了(cell重用运用)
+    func test_takeUntil() {
+        // ***** takeUntil: 从源可观察序列发出元素，直到参考可观察序列发出元素
+        print("*****takeUntil*****")
+        let sourceSequence = PublishSubject<String>()
+        let referenceSequence = PublishSubject<String>()
+        sourceSequence
+            .takeUntil(referenceSequence)
+            .subscribe(onNext: {print($0)})
+            .disposed(by: disposeBag)
+        
+        sourceSequence.onNext("kongyulu")
+        sourceSequence.onNext("yifeng")
+        sourceSequence.onNext("yisheng")
+        //referenceSequence.onNext("yuhairong") // 条件一出来,下面就走不了
+        sourceSequence.onNext("test1")
+        sourceSequence.onNext("test2")
+        sourceSequence.onNext("test3")
+    }
+    
+    func test_takeUntil2() {
+        // ***** takeUntil: 从源可观察序列发出元素，直到参考可观察序列发出元素
+        print("*****takeUntil*****")
+        let sourceSequence = PublishSubject<String>()
+        let referenceSequence = PublishSubject<String>()
+        sourceSequence
+            .takeUntil(referenceSequence)
+            .subscribe(onNext: {print($0)})
+            .disposed(by: disposeBag)
+        
+        sourceSequence.onNext("kongyulu")
+        sourceSequence.onNext("yifeng")
+        sourceSequence.onNext("yisheng")
+        
+        referenceSequence.onNext("yuhairong") // 条件一出来,下面就走不了
+        
+        sourceSequence.onNext("test1")
+        sourceSequence.onNext("test2")
+        sourceSequence.onNext("test3")
+    }
+    
+    ///从源可观察序列发出元素，直到参考可观察序列发出元素
+    /// 这个要重点,应用非常频繁  textfiled 都会有默认序列产生
+    func test_skip() {
+        // ***** skip: 从源可观察序列发出元素，直到参考可观察序列发出元素
+        print("*****skip*****")
+        Observable.of(1,2,3,4,5,6)
+        .skip(2) //直接跳过前面两个元素，即从3开始
+        .subscribe(onNext: {print($0)})
+        .disposed(by: disposeBag)
+    }
+    
+    /// 直接跳过满足条件的元素，相当于过滤作用
+    func test_skipWhile() {
+        print("*****skipWhile*****")
+        //skipWhile刚刚和takeWhile的作用相反
+        Observable.of(1, 2, 3, 4, 5, 6)
+            .skipWhile { $0 < 4 } //直接跳过满足条件的元素，相当于过滤作用（满足小于4的都跳过，即只有4，5，6）
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposeBag)
+    }
+    
+    
+    /// 抑制从源可观察序列发出元素，直到参考可观察序列发出元素
+    func test_skipUntil() {
+        // *** skipUntil: 抑制从源可观察序列发出元素，直到参考可观察序列发出元素
+        // skipUntil 作用刚刚和 takeUntil 相反
+        print("*****skipUntil*****")
+        let sourceSeq = PublishSubject<String>()
+        let referenceSeq = PublishSubject<String>()
+        
+        sourceSeq
+            .skipUntil(referenceSeq)
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposeBag)
+        
+        // 没有条件命令 下面走不了
+        sourceSeq.onNext("kongyulu")
+        sourceSeq.onNext("yifeng")
+        sourceSeq.onNext("yisheng")
+        
+        referenceSeq.onNext("yuhairong") // 条件一出来,下面就可以走了
+        
+        sourceSeq.onNext("test1")
+        sourceSeq.onNext("test2")
+        sourceSeq.onNext("test3")
+    }
+    
+}
+
+
+
+// MARK: - Rx高阶函数： 映射操作符
+extension ViewController {
+    /// 转换闭包应用于可观察序列发出的元素，并返回转换后的元素的新可观察序列。
+    func test_map() {
+        // ***** map: 转换闭包应用于可观察序列发出的元素，并返回转换后的元素的新可观察序列。
+        print("*****map*****")
+        let ob = Observable.of(1,2,3,4)
+        ob.map { (number) -> Int in
+            return number + 2
+        }
+        .subscribe(onNext: {print($0)})
+        .disposed(by: disposeBag)
+    }
+    
+    ///将可观测序列发射的元素转换为可观测序列，并将两个可观测序列的发射合并为一个可观测序列。
+    ///这也很有用，例如，当你有一个可观察的序列，它本身发出可观察的序列，
+    ///你想能够对任何一个可观察序列的新发射做出反应(序列中序列:比如网络序列中还有模型序列)
+    func test_flatmap() {
+        print("*****flatMap*****")
+        let boy = LGPlayer(score: 100)
+        let girl = LGPlayer(score: 90)
+        let player = BehaviorSubject(value: boy)
+        
+        player.asObservable()
+            .flatMap { $0.score.asObservable() } // 本身score就是序列 模型就是序列中的序列
+            .subscribe(onNext: {print($0)})
+            .disposed(by: disposeBag)
+        
+        boy.score.onNext(60)
+        player.onNext(girl)
+        boy.score.onNext(50)
+        boy.score.onNext(40)//  如果切换到 flatMapLatest 就不会打印
+        girl.score.onNext(10)
+        girl.score.onNext(0)
+    }
+    
+    /// flatMap和flatMapLatest的区别是，flatMapLatest只会从最近的内部可观测序列发射元素
+    /// flatMapLatest实际上是map和switchLatest操作符的组合。
+    func test_flatMapLatest() {
+        print("*****flatMapLatest*****")
+        let boy = LGPlayer(score: 100)
+        let girl = LGPlayer(score: 90)
+        let player = BehaviorSubject(value: boy)
+        
+        player.asObservable()
+            .flatMapLatest { $0.score.asObservable() } // 本身score就是序列 模型就是序列中的序列
+            .subscribe(onNext: {print($0)})
+            .disposed(by: disposeBag)
+        
+        boy.score.onNext(60)
+        player.onNext(girl)
+        boy.score.onNext(50)
+        boy.score.onNext(40)//  如果切换到 flatMapLatest 就不会打印
+        girl.score.onNext(10)
+        girl.score.onNext(0)
+    }
+    
+    ///从初始就带有一个默认值开始，然后对可观察序列发出的每个元素应用累加器闭包，并以单个元素可观察序列的形式返回每个中间结果
+    func test_scan() {
+        print("*****scan*****")
+        Observable.of(10,100,1000)
+            .scan(2) { aggregateValue, newValue in
+                aggregateValue + newValue // 10 + 2 , 100 + 10 + 2 , 1000 + 100 + 2
+        }
+        .subscribe(onNext: {print($0)})
+        .disposed(by: disposeBag)
+    }
+}
+
+
+
+
 
 
 
