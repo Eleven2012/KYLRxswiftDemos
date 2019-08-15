00b656ad5ac4c0ed5a2d0a3c62bdb842b6f277ef
yulu.kong <yulu.kong@ubtrobot.com>
2019-08-15 15:24:38 +0800

    增加web桥接demo

---
+++ 003-kylDSbrigeDemo/kyl_DSBrigeDemo/Pods/Target Support Files/Pods-kyl_DSBrigeDemoUITests/Pods-kyl_DSBrigeDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kyl_DSBrigeDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_kyl_DSBrigeDemoUITests
+@end
---
+++ 003-kylDSbrigeDemo/kyl_DSBrigeDemo/kyl_DSBrigeDemoTests/kyl_DSBrigeDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  kyl_DSBrigeDemoTests.swift
+//  kyl_DSBrigeDemoTests
+//
+//  Created by yulu kong on 2019/8/15.
+//  Copyright © 2019 yulu kong. All rights reserved.
+//
+
+import XCTest
+@testable import kyl_DSBrigeDemo
+
+class kyl_DSBrigeDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
