7f3097de28fff00affbf367747b8d0751c1babd2
yulu.kong <yulu.kong@ubtrobot.com>
2019-08-15 17:09:08 +0800

    新增webview

---
+++ 003-kylDSbrigeDemo/kyl_DSBrigeDemo/kyl_DSBrigeDemo/dsbrige/KYLJSApiTest.swift
@@ -0,0 +1,9 @@
+//
+//  KYLJSApiTest.swift
+//  kyl_DSBrigeDemo
+//
+//  Created by yulu kong on 2019/8/15.
+//  Copyright © 2019 yulu kong. All rights reserved.
+//
+
+import Foundation
---
+++ 003-kylDSbrigeDemo/kyl_DSBrigeDemo/kyl_DSBrigeDemo/dsbrige/KYLWebViewVC.swift
@@ -0,0 +1,30 @@
+//
+//  KYLWebViewVC.swift
+//  kyl_DSBrigeDemo
+//
+//  Created by yulu kong on 2019/8/15.
+//  Copyright © 2019 yulu kong. All rights reserved.
+//
+
+import UIKit
+
+class KYLWebViewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
