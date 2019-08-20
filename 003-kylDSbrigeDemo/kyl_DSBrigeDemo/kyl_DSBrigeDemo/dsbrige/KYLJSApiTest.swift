76402e56494dc036e3a072e22c07a44d4a64d719
yulu.kong <yulu.kong@ubtrobot.com>
2019-08-20 14:11:47 +0800

    完成dsbridge实例代码

--- 003-kylDSbrigeDemo/kyl_DSBrigeDemo/kyl_DSBrigeDemo/dsbrige/KYLJSApiTest.swift
+++ 003-kylDSbrigeDemo/kyl_DSBrigeDemo/kyl_DSBrigeDemo/dsbrige/KYLJSApiTest.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+typealias JSCallback = (String,Bool) -> Void
+
+class KYLJSApi: NSObject {
+    
+    private var value = 10
+    private var callback:JSCallback?
+    private var timer:Timer?
+    
+}
+
+extension KYLJSApi {
+    @objc public func testSync(_ arg:String) -> String {
+        debugPrint("\(#function)")
+        return String(format: "%@[Swift sync call:%@]", arg, "test")
+    }
+    
+    @objc public func testAync(_ arg:String, handler: JSCallback) {
+        debugPrint("\(#function)")
+        handler("\(arg) [Swift async call test] ",true)
+    }
+    
+    @objc public func testNoArgSync(_ args:Dictionary<String,Any>) -> String {
+        debugPrint("\(#function)")
+        return "带有dic参数的方法"
+    }
+    
+    @objc public func callProgress(_ args:Dictionary<String,Any>, handler:@escaping JSCallback) {
+        debugPrint("\(#function)")
+        callback  = handler
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(feedbackValue), userInfo: nil, repeats: true)
+    }
+    
+    @objc public func feedbackValue() {
+        if let handler = callback {
+            if value > 0 {
+                handler("\(value)",false)
+                value -= 1
+            }
+            else {
+                handler("\(value)",true)
+            }
+        }
+    }
+}
--- 003-kylDSbrigeDemo/kyl_DSBrigeDemo/kyl_DSBrigeDemo/dsbrige/KYLWebViewVC.swift
+++ 003-kylDSbrigeDemo/kyl_DSBrigeDemo/kyl_DSBrigeDemo/dsbrige/KYLWebViewVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import dsBridge
+import SnapKit
 
 class KYLWebViewVC: UIViewController {
 
@@ -14,8 +16,104 @@ class KYLWebViewVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupUI()
     }
     
 
+    private var webview:DWKWebView = {
+        let webview:DWKWebView = DWKWebView.init()
+        return webview
+    }()
 
+}
+
+// MARK: - 设置Webview
+extension KYLWebViewVC {
+    private func setupUI() {
+        setupWebviewUI()
+    }
+    
+    private func setupWebviewUI() {
+        view.addSubview(webview)
+        webview.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+        #if DEBUG
+        webview.setDebugMode(true)
+        #endif
+        webview.customJavascriptDialogLabelTitles(["alertTitle":"Nofification","alertBtn":"OK"])
+        webview.navigationDelegate = self
+        
+        guard let htmlPath = Bundle.main.path(forResource: "test", ofType: "html"),
+        let htmlContent = (try? String.init(contentsOfFile: htmlPath, encoding: String.Encoding.utf8)) else {
+            return
+        }
+        
+        webview.loadHTMLString(htmlContent, baseURL: URL(fileURLWithPath: Bundle.main.bundlePath))
+    }
+}
+
+// MARK: - JS 调用 原生
+extension KYLWebViewVC {
+    private func addJSMethod() {
+        webview.addJavascriptObject(KYLJSApi(), namespace: nil)
+        //增加命名空间, JS在调用的时候可以用 swift.methodName 方便管理功能模块可增强阅读
+        webview.addJavascriptObject(KYLJSApi(), namespace: "swift")
+    }
+}
+
+// MARK: - 原生 调用 JS
+extension KYLWebViewVC {
+    
+    // 字典转json字符串
+    private func dicToString(_ dic:[String : Any]) -> String? {
+        let data = try? JSONSerialization.data(withJSONObject: dic, options: [])
+        let str = String(data: data!, encoding: String.Encoding.utf8)
+        return str
+    }
+    
+    private func nativeCallJS() {
+        //原生调用js的addValue方法，参数是[3,4],返回值是value
+        webview.callHandler("addValue", arguments: [3,4]) { (value) in
+            debugPrint("\(#function) \(String(describing: value))")
+        }
+        
+        //拼接字符串
+        webview.callHandler("append", arguments: ["I", "love","you"]) { (value) in
+            debugPrint("\(#function) \(String(describing: value))")
+        }
+        
+        //传递json数据
+        let jsonStr = dicToString(["name":"kongyulu","sex":"male"])
+        webview.callHandler("showJson", arguments: [jsonStr ?? ""]) { (value) in
+            debugPrint("\(#function) \(String(describing: value))")
+        }
+        
+        //收到result 转为json
+        webview.callHandler("showResult", arguments:  [jsonStr ?? ""]) { (value) in
+            debugPrint("\(#function) \(String(describing: value))")
+        }
+        
+        //带有命名空间的方法
+        webview.callHandler("sync.addValue", arguments: [5,6]) { (value) in
+            debugPrint("\(#function) \(String(describing: value))")
+        }
+        
+        //测试js里面是否有某个方法
+        webview.hasJavascriptMethod("addValue") { (isHas) in
+            debugPrint("isHas = \(isHas)")
+        }
+        
+        //如果H5调用了window.close方法就会监听到
+        webview.setJavascriptCloseWindowListener {
+            debugPrint("\(#function) 监听到关闭H5页面")
+        }
+    }
+}
+
+// MARK: - WKNavigationDelegate
+extension KYLWebViewVC: WKNavigationDelegate{
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        debugPrint("\(#function)")
+    }
 }
