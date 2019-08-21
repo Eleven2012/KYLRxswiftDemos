0b0c505c2540a0bef7e7d3d6a2d38b69a8445443
yulu.kong <yulu.kong@ubtrobot.com>
2019-08-21 12:15:49 +0800

    increase pod

---
+++ 004-kylRxVideoPlayer/kylRxVideoPlayer/Pods/Target Support Files/Pods-kylRxVideoPlayer-kylRxVideoPlayerUITests/Pods-kylRxVideoPlayer-kylRxVideoPlayerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kylRxVideoPlayer_kylRxVideoPlayerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_kylRxVideoPlayer_kylRxVideoPlayerUITests
+@end
