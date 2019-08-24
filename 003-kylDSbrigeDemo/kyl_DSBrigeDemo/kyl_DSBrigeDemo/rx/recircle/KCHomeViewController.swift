9597428b60d23b4ef46ff009e8f97960bf0f4e29
yulu.kong <yulu.kong@ubtrobot.com>
2019-08-24 09:06:16 +0800

    修改测试文件

--- 003-kylDSbrigeDemo/kyl_DSBrigeDemo/kyl_DSBrigeDemo/rx/recircle/KCHomeViewController.swift
+++ 003-kylDSbrigeDemo/kyl_DSBrigeDemo/kyl_DSBrigeDemo/rx/recircle/KCHomeViewController.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2019 yulu kong. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+
+
+class KCHomeViewController: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("\(#function)*****首页控制器出现了show:RxSwift的引用计数: \(RxSwift.Resources.total)")
+        print("****************************************")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("\(#function)*****首页控制器消失了dismiss:RxSwift的引用计数: \(RxSwift.Resources.total)")
+        print("****************************************")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .gray
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        print("\(#function)*****首页控制touchesBegan:RxSwift的引用计数: \(RxSwift.Resources.total)")
+        print("****************************************")
+        
+        let vc = KCTestMemoryViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
