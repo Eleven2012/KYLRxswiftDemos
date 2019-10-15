fefd040fd4861058fa19ab13a571ff92cedd89db
yulu.kong <yulu.kong@ubtrobot.com>
2019-10-15 16:00:57 +0800

    new playground demo

---
+++ 021-kylDrawDemo/kylDrawDemo/kylDrawDemo/draw/KDrawPanView.swift
@@ -0,0 +1,21 @@
+//
+//  KDrawPanView.swift
+//  kylDrawDemo
+//
+//  Created by yulu kong on 2019/10/11.
+//  Copyright © 2019 yulu kong. All rights reserved.
+//
+
+import UIKit
+
+class KDrawPanView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
